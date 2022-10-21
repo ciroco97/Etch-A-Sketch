e051c7817ff1c33a466dec992f475608d2039c6b
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-21 16:50:52 -0300

    Add functions, and basic css

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
+let color = "black";
 
 function createBoard(valor) {
     const container = document.getElementById("container");
     const squares = container.querySelectorAll("div");
     squares.forEach((div) => div.remove());
     const value = valor * valor;
-    const sendValue = document.getElementById("btn");
-    sendValue.addEventListener("click", createBoard);
     container.style.gridTemplateColumns = `repeat(${valor}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${valor}, 1fr)`;
 
-
-    for (let i = 0; i < value; i++) {
-        let divs = document.createElement("div");
-        divs.classList.add('inside');
-        container.appendChild(divs);
-    };
+    if (valor < 101 && valor > 1) {
+        for (let i = 0; i < value; i++) {
+            let divs = document.createElement("div");
+            divs.classList.add('inside');
+            divs.addEventListener("mouseover", whichColor);
+            container.appendChild(divs);
+        };
+    } else {
+        alert("Please insert a value between 1 and 100.");
+    }
 }
 
+createBoard(16);
 
+function whichColor() {
+    if (color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() *  360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+};
+
+function setColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    const container = document.getElementById("container");
+    const squares = container.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
 
